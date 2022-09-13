a3331ee7d46376f286c2610c166f15ac5ff5e5f5
Araya <arayaa909@gmail.com>
2022-09-13 00:39:17 -0400

    WeatherAPI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WEATHER'`, () => {
+  it(`should have as title 'weatherAPI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WEATHER');
+    expect(app.title).toEqual('weatherAPI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('WEATHER app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('weatherAPI app is running!');
   });
 });
